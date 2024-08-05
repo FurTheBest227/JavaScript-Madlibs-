bc85d3abf1798e67107ee186caa5d6e63ccc67ed
Furqan Ahmed <zerogin227@gmail.com>
2024-08-05 13:34:40 +0000

    Assignment

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let MyNumber = 10
 let MyExpression = "5 + 5 =";
 let IsHappy = "false"
 console.log("Hi, do you like" ,Myword,"hopefully NOT, but we all have different opinions, But I aksed lots of people if they are happy coming to school and they say" ,IsHappy,"like why do we have to know why" , MyExpression,MyNumber,"This is just some of the reasons School sucks(P.S. I dont really think this)" )
+prompt("Do you like School?")
+
+//When running the code, my Sentence should be at the top because it gives me a error, that prompt
+// is not defined, but it should still work when going live and running the code.
