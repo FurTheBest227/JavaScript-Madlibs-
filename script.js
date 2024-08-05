ae0028014f8b31dc4793aec9fd1ab9e1903ca34b
Furqan Ahmed <zerogin227@gmail.com>
2024-08-05 13:29:44 +0000

    Assignment

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let Myword = "School"
-let MyNumber = 24
-let MyExpression = 5 + 5;
+let MyNumber = 10
+let MyExpression = "5 + 5 =";
 let IsHappy = "false"
-
-
-console.log(;
+console.log("Hi, do you like" ,Myword,"hopefully NOT, but we all have different opinions, But I aksed lots of people if they are happy coming to school and they say" ,IsHappy,"like why do we have to know why" , MyExpression,MyNumber,"This is just some of the reasons School sucks(P.S. I dont really think this)" )
